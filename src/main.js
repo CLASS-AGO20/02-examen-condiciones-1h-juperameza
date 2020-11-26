1580e17669e2454bb9fd9d7beb0a845ec966d922
juperameza <juperameza@hotmail.com>
2020-11-26 15:24:22 -0600

    added function costoRenta

--- src/main.js
+++ src/main.js
@@ -9,7 +9,24 @@ convertir(pesos){
     }
     return convertion;
 }
+costoRenta(kilometros){
+    let costo;
+    if(kilometros<50){
+        costo=kilometros*3;
+    }
+    else if(kilometros>=50 && kilometros<100){
+        costo=kilometros*5;
+    }
+    else if(kilometros>=100&&kilometros<200){
+        costo=kilometros*6;
+    }
+    else{
+        costo=kilometros*6.5;
+    }
+    return costo;
+}
 }
 
 let app = new App();
 console.log(app.convertir(2000));
+console.log(app.costoRenta(200));
