73aaf9b95cd1a4ee71d387fba6bc53da77b0f7a2
juperameza <juperameza@hotmail.com>
2020-11-26 15:17:08 -0600

    added function convertir

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 export default class App {
-
+convertir(pesos){
+    let convertion;
+    if(pesos<2000){
+        convertion=pesos*20.5;
+    }
+    else{
+        convertion=pesos*22.7;
+    }
+    return convertion;
+}
 }
 
 let app = new App();
+console.log(app.convertir(2000));
